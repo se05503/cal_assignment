55a6476af2eaaa73bf5319b816b8ab4cf7c4acc0
SEYOUNG PARK <seyeongb18@gmail.com>
2024-03-11 19:18:53 +0900

    깃 브랜치 연습

--- lv4.kt
+++ lv4.kt
@@ -1,32 +1,6 @@
 package com.example.calculator_assignment
 
-import com.example.calculatorassignment.AddOperation
-import com.example.calculatorassignment.Calculator
-import com.example.calculatorassignment.DivideOperation
-import com.example.calculatorassignment.MultiplyOperation
-import com.example.calculatorassignment.SubstractOperation
-
-//fun main() {
-////    val addOperation = AddOperation()
-//    var addCal = Calculator(AddOperation())
-//    println("계산 결과 : ${addCal.calculate(3,5)}")
-//
-//    var subCal = Calculator(SubstractOperation())
-//    println("계산 결과 : ${subCal.calculate(3,5)}")
-//
-//    var divCal = Calculator(DivideOperation())
-//    println("계산 결과 : ${divCal.calculate(3,5)}")
-//
-//    var mulCal = Calculator(MultiplyOperation())
-//    println("계산 결과 : ${mulCal.calculate(3,5)}")
-//
-//    val myStack = MyStack()
-//    val calResult = myStack.getPostFixExpressionOperation("(2+4)*4/2*12")
-//    println("계산 결과 : ${calResult}")
-//
-//}
-
 fun main() {
  저는 수정중입니다!저는 modify 브랜치에 이 코드가 들어있음 좋겠습니다!
-    
+
 }
