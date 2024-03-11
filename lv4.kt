d56b0543edfd8cd6e5455bbc9e03e0e39f32b1e7
SEYOUNG PARK <seyeongb18@gmail.com>
2024-03-11 20:43:14 +0900

    modify 브랜치 수정

--- lv4.kt
+++ lv4.kt
@@ -1,6 +1,26 @@
 package com.example.calculator_assignment
 
-fun main() {
- 저는 수정중입니다!저는 modify 브랜치에 이 코드가 들어있음 좋겠습니다!
 
+import com.example.calculatorassignment.AddOperation
+import com.example.calculatorassignment.Calculator
+import com.example.calculatorassignment.DivideOperation
+import com.example.calculatorassignment.MultiplyOperation
+import com.example.calculatorassignment.SubstractOperation
+
+fun main() {
+ val addCalc = Calculator(AddOperation())
+ println("10더하기 20 결과는 : ${addCalc.operate(10, 20)} 입니다")
+
+ val minusCalc = Calculator(SubstractOperation())
+ println("20빼기 10 결과는 : ${minusCalc.operate(20, 10)} 입니다")
+
+ val multipleCalc = Calculator(MultiplyOperation())
+ println("10곱하기 20 결과는 : ${multipleCalc.operate(10, 20)} 입니다")
+
+ val divideCalc = Calculator(DivideOperation())
+ println("20나누기 10 결과는 : ${divideCalc.operate(20, 10)} 입니다")
+
+ val myStack = MyStack()
+ val calResult = myStack.getPostFixExpressionOperation("(2+4) * 4 / 2 * 12")
+ println("복합 연산 계산 결과 : ${calResult}")
 }
