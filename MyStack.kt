3765cb496c42da8294faa47e4b887fe47f8aaf19
SEYOUNG PARK <seyeongb18@gmail.com>
2024-03-14 14:09:21 +0900

    lv5도 정리완료

--- MyStack.kt
+++ MyStack.kt
@@ -3,80 +3,99 @@ package com.example.calculator_assignment
 import java.util.Stack
 
 class MyStack {
-    fun getPostFixExpressionOperation(expression: String): Int {
-        val stack = Stack<String>()
-        val tokenizedArray = toTokenizedArray(expression)
-        var postFixExpression = ""
-        for (e in tokenizedArray) {
-            when(e) {
-                "+","-","*","/" -> {
+    companion object {
+        fun getPostFixExpressionOperation(inputStr: String): String {
+            var strArr = strToArr(inputStr)
+            val stack = Stack<String>()
+            var answer: String = ""
+            for (e in strArr) {
+                when (e) {
+                    "+", "-", "*", "/" -> {
+                        while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(e)) {
+                            answer += stack.pop() + " "
+                        }
+                        stack.push(e)
+                    }
 
-                }
-                "(" -> {
+                    "(" -> {
+                        stack.push(e)
+                    }
 
-                }
-                ")" -> { // 나는 modify branch 이다..........
+                    ")" -> {
+                        while (stack.peek() != "(") {
+                            answer += stack.pop() + " "
+                        }
+                        stack.pop()
+                    }
 
-                }
-                else -> {
-
+                    else -> {
+                        answer += "$e "
+                    }
                 }
             }
-//            if (e == "*" || e == "/" || e == "+" || e == "-") {
-//                if (stack != null && stack.peek() != "(") {
-//                    postFixExpression += stack.pop() + " "
-//                    stack.push(e)
-//                } else stack.push(e)
-//            } else if (e == "(") {
-//                stack.push(e)
-//            } else if (e == ")") {
-//                postFixExpression += stack.pop() + " "
-//                stack.pop()
-//            } else {
-//                postFixExpression += "$e "
-//            }
-        }
-        if (stack != null) {
-            postFixExpression += stack.pop()
-        }
-        return getIntValue(postFixExpression)
-    }
 
-    fun toTokenizedArray(str: String): Array<String> {
-        var strCopy = str.replace("(", "( ")
-        strCopy = strCopy.replace(")", " )")
-        return strCopy.split(" ").toTypedArray()
-    }
+            while (!stack.isEmpty()) {
+                answer += stack.pop() + " "
+            }
 
-    fun getIntValue(postFixExpression: String): Int {
-        var arrayStr = postFixExpression.split(" ")
-        var stack = Stack<String>()
-        var result = 0
-        for (e in arrayStr) {
-            if (e == "+") {
-                val n1 = stack.pop().toInt()
-                val n2 = stack.pop().toInt()
-                result = n1 + n2
-                stack.push((n1 + n2).toString())
-            } else if (e == "-") {
-                val n1 = stack.pop().toInt()
-                val n2 = stack.pop().toInt()
-                result = n1 - n2
-                stack.push((n1 - n2).toString())
-            } else if (e == "*") {
-                val n1 = stack.pop().toInt()
-                val n2 = stack.pop().toInt()
-                result = n1 * n2
-                stack.push((n1 * n2).toString())
-            } else if (e == "/") {
-                val n1 = stack.pop().toInt()
-                val n2 = stack.pop().toInt()
-                result = n2 / n1
-                stack.push((n2 / n1).toString())
-            } else {
-                stack.push(e)
+            return answer
+        }
+
+        fun getAnswer(str: String): Int {
+            // str : 컴퓨터가 이해할 수 있는 후위 연산 (2+4)*6/3 → 2 4 - 6 * 3 /
+            val strArr = str.split(" ").toTypedArray() // 일단 배열에 익숙해져보자
+            val stack = Stack<String>()
+            var answer = 0
+            for (e in strArr) {
+                when (e) {
+                    "+" -> {
+                        val num1 = stack.pop().toInt()
+                        val num2 = stack.pop().toInt()
+                        answer = num1 + num2
+                        stack.push(answer.toString())
+                    }
+
+                    "-" -> {
+                        val num1 = stack.pop().toInt() //4
+                        val num2 = stack.pop().toInt() //2
+                        answer = -(num1 - num2)
+                        stack.push(answer.toString())
+                    }
+
+                    "*" -> {
+                        val num1 = stack.pop().toInt()
+                        val num2 = stack.pop().toInt()
+                        answer = num1 * num2
+                        stack.push(answer.toString())
+                    }
+
+                    "/" -> { // 3 6 /
+                        val num1 = stack.pop().toInt() // 6
+                        val num2 = stack.pop().toInt() // 3
+                        answer = num2 / num1 // 3/6
+                        stack.push(answer.toString())
+                    }
+
+                    else -> {
+                        stack.push(e)
+                    }
+                }
+            }
+            return answer
+        }
+
+        fun strToArr(s: String): Array<String> {
+            var tempStr = s.replace("(", "( ")
+            tempStr = tempStr.replace(")", " )")
+            return tempStr.split(" ").toTypedArray()
+        }
+
+        fun precedence(operation: String): Int {
+            return when (operation) {
+                "*", "/" -> 2
+                "+", "-" -> 1
+                else -> 0
             }
         }
-        return result
     }
 }
